71a69769481367b980f5eec64dae335cc3e56bda
jeong-gumin <jgm@paytalab.com>
2023-04-12 00:45:22 +0900

    feat : 회원 간단 정보 관련 도메인 객체 todo 구현

--- src/main/kotlin/com/example/ecommerce/domain/member/model/MemberSimple.kt
+++ src/main/kotlin/com/example/ecommerce/domain/member/model/MemberSimple.kt
@@ -1,11 +1,31 @@
 package com.example.ecommerce.domain.member.model
 
 import com.example.ecommerce.domain.member.Member
+import com.example.ecommerce.domain.member.MemberAddress
+import com.example.ecommerce.domain.member.MemberId
+import java.time.LocalDate
 
-class MemberSimple {
+data class MemberSimple(
+    val id : MemberId,
+    val name: String,
+    val nickname: String,
+    val birth: LocalDate,
+    val email: String,
+    val phone: String?,
+    val tel: String?,
+    val address: MemberAddress
+) {
     companion object {
-        fun from(it: Member) : MemberSimple{
-            TODO("Not yet implemented")
+        fun from(member: Member) : MemberSimple{
+            return MemberSimple(
+                id = member.id,
+                name = member.name,
+                nickname = member.nickname,
+                birth = member.birth,
+                email = member.email,
+                phone = member.phone,
+                tel = member.tel,
+                address = member.memberAddress)
         }
     }
 }
