b528bc0fc1e759ccd45ebf521fcf97710034e6bf
jeong-gumin <jgm@paytalab.com>
2023-04-12 00:44:29 +0900

    feat : account domain 수정
    - member id 누락 부분 추가
    - 비밀번호 존재하는지 프로퍼티 추가

--- src/main/kotlin/com/example/ecommerce/domain/account/Account.kt
+++ src/main/kotlin/com/example/ecommerce/domain/account/Account.kt
@@ -1,9 +1,17 @@
 package com.example.ecommerce.domain.account
 
+import com.example.ecommerce.domain.member.MemberId
 import com.example.ecommerce.global.domain.AbstractAggregate
 
 class Account(
     val accountType: AccountType,
+    val memberId: MemberId,
     val loginId: String,
     val password: String?
-) : AbstractAggregate<AccountId>()
+) : AbstractAggregate<AccountId>() {
+
+    val mustHavePassword: Boolean
+        get() {
+            return this.password!=null
+        }
+}
