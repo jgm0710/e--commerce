bab0770eceb58e64d115b5652d13b912c2ec17c8
jeong-gumin <jgm@paytalab.com>
2023-04-14 00:17:52 +0900

    feat : swagger 설정 추가

--- src/main/kotlin/com/example/ecommerce/adepter/in/web/config/SwaggerConfig.kt
+++ src/main/kotlin/com/example/ecommerce/adepter/in/web/config/SwaggerConfig.kt
@@ -2,8 +2,6 @@ package com.example.ecommerce.adepter.`in`.web.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
 import springfox.documentation.spi.DocumentationType
@@ -12,13 +10,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @Configuration
 @EnableSwagger2
-class SwaggerConfig : WebMvcConfigurer {
-    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
-        registry.addResourceHandler("/swagger-ui/**")
-            .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
-        registry.addResourceHandler("/swagger-resources/**")
-            .addResourceLocations("classpath:/META-INF/resources/swagger-resources/")
-    }
+class SwaggerConfig  {
 
     @Bean
     fun api(): Docket {
