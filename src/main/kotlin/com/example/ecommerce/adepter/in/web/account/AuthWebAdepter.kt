7bb876491b6cb60e458ea6648a409c0033ef5b69
jeong-gumin <jgm@paytalab.com>
2023-05-17 00:34:24 +0900

    feat : auth 문서화 추가

--- src/main/kotlin/com/example/ecommerce/adepter/in/web/account/AuthWebAdepter.kt
+++ src/main/kotlin/com/example/ecommerce/adepter/in/web/account/AuthWebAdepter.kt
@@ -3,6 +3,8 @@ package com.example.ecommerce.adepter.`in`.web.account
 import com.example.ecommerce.adepter.`in`.web.account.request.SignInRequest
 import com.example.ecommerce.adepter.`in`.web.account.response.AuthInfoResponse
 import com.example.ecommerce.application.port.account.`in`.SignInUseCase
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -10,11 +12,13 @@ import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
 
+@Api(tags = ["Auth API"])
 @RestController
 class AuthWebAdepter(
     private val signInUseCase: SignInUseCase
 ) {
 
+    @ApiOperation("Sign in")
     @PostMapping("auth/sign-in")
     @ResponseStatus(HttpStatus.OK)
     fun signIn(
